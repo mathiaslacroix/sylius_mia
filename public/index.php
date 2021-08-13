57706972269264705f9e33723b7247c6c9aa1ab9
mathiaslacroix <lacroix.mathias@free.fr>
2021-08-13 12:32:15 +0200

    Modification index.php Work In Progress

--- public/index.php
+++ public/index.php
@@ -1,27 +1,27 @@
-<?php
+<?php echo "<h1>Work In Progress.</H1>";
 
-use App\Kernel;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
+// use App\Kernel;
+// use Symfony\Component\ErrorHandler\Debug;
+// use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+// require dirname(__DIR__).'/config/bootstrap.php';
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+// if ($_SERVER['APP_DEBUG']) {
+//     umask(0000);
 
-    Debug::enable();
-}
+//     Debug::enable();
+// }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-}
+// if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+//     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+// }
 
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
+// if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+//     Request::setTrustedHosts([$trustedHosts]);
+// }
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+// $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+// $request = Request::createFromGlobals();
+// $response = $kernel->handle($request);
+// $response->send();
+// $kernel->terminate($request, $response);
